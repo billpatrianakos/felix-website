6ad2256ffa545049be115123674bc69fbeb60bc8
Bill Patrianakos <bill@billpatrianakos.me>
2018-08-06 16:46:16 -0500

    Add support for image loaders.

--- webpack.config.js
+++ webpack.config.js
@@ -50,11 +50,22 @@ module.exports = {
             }
           }
         ]
+      },
+      {
+        test: /\.(png|jp(e*)g|svg)$/,  
+        use: [{
+          loader: 'url-loader',
+          options: { 
+            limit: 8000, // Convert images < 8kb to base64 strings
+            name: 'images/[hash]-[name].[ext]'
+          }
+        }]
       }
     ]
   },
   devServer: {
     contentBase: path.resolve(__dirname, 'client'),
+    historyApiFallback: true,
     port: 8080,
     open: true,
     proxy: {
