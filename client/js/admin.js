274ee2d8014f461c4d592aade84965701c62dba8
Bill Patrianakos <bill@billpatrianakos.me>
2018-08-12 19:27:46 -0500

    Update admin entry with album route code.

--- client/js/admin.js
+++ client/js/admin.js
@@ -2,27 +2,35 @@
 // ========================================
 
 import '../less/admin.less'; // Removed by Webpack at compile time
-import React, {Component} from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-import { Helmet } from 'react-helmet';
-import AdminHome from './pages/AdminHome';
-import Login from './pages/Login';
+import React, {Component}                       from 'react';
+import ReactDOM                                 from 'react-dom';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+import AdminHome                                from './pages/AdminHome';
+import Login                                    from './pages/Login';
+import AdminAlbum                               from './pages/AdminAlbum';
+import AdminAlbumNew                            from './pages/AdminAlbumNew';
 
 class App extends Component {
   render() {
     return (
       <Router basename="/admin">
         <div className="grid">
-          <Helmet>
-            <title>Admin</title>
-          </Helmet>
           <ul>
             <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
+            <li>
+              <Link to="/albums">Albums</Link>
+              <ul>
+                <li><Link to="/albums/new">New Album</Link></li>
+              </ul>
+            </li>
           </ul>
           <Route exact path="/" component={AdminHome}/>
           <Route path="/login" component={Login} />
+          <Switch>
+            <Route exact path="/albums" component={AdminAlbum} />
+            <Route path="/albums/new" component={AdminAlbumNew} />
+          </Switch>
         </div>
       </Router>
     );
