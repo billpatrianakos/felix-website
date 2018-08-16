f54fa98257b77a210519dd2fda87b5858995e1cc
Bill Patrianakos <bill@billpatrianakos.me>
2018-08-16 16:47:46 -0500

    Refactor how Bookshelf plugins are enabled into a one-liner.

--- server/config/db.js
+++ server/config/db.js
@@ -4,9 +4,6 @@
 const knex      = require('knex')(require(__dirname + '/../knexfile')[process.env.NODE_ENV || 'development']);
 const Bookshelf = require('bookshelf')(knex);
 
-Bookshelf.plugin('registry');
-Bookshelf.plugin('virtuals');
-Bookshelf.plugin('visibility');
-Bookshelf.plugin('pagination');
+Bookshelf.plugin(['registry', 'virtuals', 'visibility', 'pagination']);
 
 module.exports = Bookshelf;
