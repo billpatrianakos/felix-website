6e29820d134c169483b04a8914d7e0af7bef473a
Bill Patrianakos <bill@billpatrianakos.me>
2018-08-16 16:49:10 -0500

    Add beforeSave callbacks to Post model that parses markdown and creates a post slug from the title if none is already specified.

--- server/models/post.js
+++ server/models/post.js
@@ -1,13 +1,30 @@
 // Post model
 // ==========
-const Bookshelf = require(__dirname + '/../config/db');
+const Bookshelf   = require(__dirname + '/../config/db');
+const MarkdownIt  = require('markdown-it');
+const md          = new MarkdownIt();
 require('./user');
 
 let Post = Bookshelf.Model.extend({
+  initialize: function() {
+    this.constructor.__super__.initialize.apply(this, arguments);
+    this.on('saving', this.parseMarkdown, this);
+    this.on('saving', this.createSlug, this);
+  },
   tableName: 'posts',
   hasTimestamps: true,
   author: function() {
     return this.belongsTo('User');
+  },
+  parseMarkdown: function() {
+    let parsedBody = md.render(this.get('body_markdown'));
+    this.set('body', parsedBody);
+  },
+  createSlug: function() {
+    if (this.get('slug') === '') {
+      let slug = this.get('title').toString().toLowerCase().replace(/\s+/g, '-').replace(/[^\w\-]+/g, '').replace(/\-\-+/g, '-').replace(/^-+/, '').replace(/-+$/, '');
+      this.set('slug', slug);
+    }
   }
 });
 
