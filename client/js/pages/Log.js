ebb2f96f487c2ece55aeea1047edbba8019b0b3d
Bill Patrianakos <bill@billpatrianakos.me>
2018-09-28 13:27:51 -0500

    Fix issue where single posts and post lists were not correctly identified in conditionals consistently.

--- client/js/pages/Log.js
+++ client/js/pages/Log.js
@@ -7,6 +7,8 @@ import { Link } from 'react-router-dom';
 import moment from 'moment';
 import trimHTML from 'trim-html';
 import striptags from 'striptags';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
 
 class LogContainer extends Component {
   constructor(props) {
@@ -50,8 +52,8 @@ class LogContainer extends Component {
         <Helmet>
           <title>Log | Felix & Friends</title>
         </Helmet>
-        <h2 className="page-title">The Log</h2>
-        { this.state.posts.map((post, i) => <Post slug={post.slug} title={post.title} created_at={post.created_at} body={post.body} author={post.author} singlePost={this.isSinglePost()} key={i} />) }
+        { this.isSinglePost() ? '' : <h2 className="page-title">The Log</h2> }
+        { this.state.posts.map((post, i) => <Post slug={post.slug} title={post.title} created_at={post.created_at} body={post.body} author={post.author} singlePost={this.isSinglePost()} featured={post.featured} key={i} />) }
       </section>
     );
   }
@@ -60,15 +62,17 @@ class LogContainer extends Component {
 class Post extends Component {
   render() {
     let postBody = this.props.singlePost ? this.props.body : `<p>${trimHTML(striptags(this.props.body), { limit: 450, suffix: '...' }).html}</p>`;
+    const featuredIcon = this.props.featured ? <FontAwesomeIcon icon={faStar} /> : '';
     return (
-      <div className={`post ${this.props.isSinglePost ? '' : 'post-list-item'}`}>
+      <div className={this.props.singlePost ? 'post' : 'post-list-item'}>
         <header>
-          <h2 className={this.isSinglePost ? '' : 'h3'}>{ this.props.singlePost ? this.props.title : <Link to={`/log/${this.props.slug}`}>{this.props.title}</Link> }</h2>
+          <h2 className={this.props.singlePost ? 'page-title' : 'h3'}>{ this.props.singlePost ? this.props.title : <Link to={`/log/${this.props.slug}`}>{this.props.title}</Link> }</h2>
           <p className="byline">
-            <time dateTime={this.props.created_at}>{moment(this.props.created_at).format('MMMM Do YYYY')}</time> by {this.props.author.first_name}
+            <time dateTime={this.props.created_at}>{moment(this.props.created_at).format('MMMM Do YYYY')}</time> by {this.props.author.first_name} {featuredIcon}
           </p>
         </header>
         <div className="post-body" dangerouslySetInnerHTML={{__html: postBody}} />
+        { this.isSinglePost ? '' : <hr /> }
       </div>
     );
   }
